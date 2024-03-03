2534c1f88e789a9d413ae24e729f4efdd8b65a5e
Dazank <adriandimoko2015@gmail.com>
2024-03-03 16:12:45 +0100

    upload edited

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../build/*html"],
+  theme: {
+    extend: { fontFamily:{'prata':["prata","sans-serif"],
+  'inter':["inter","sans-serif"] }},
+  },
+
+  plugins: [],
+
+}
+
